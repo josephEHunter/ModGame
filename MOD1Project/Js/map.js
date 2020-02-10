45a2642b6bb434743b8c194213d0d8dd75c5c0df
Joseph E. Hunter <52681467+josephEHunter@users.noreply.github.com>
2020-02-10 15:31:35 -0500

    Add files via upload

--- MOD1Project/Js/map.js
+++ MOD1Project/Js/map.js
@@ -1,5 +1,4 @@
 
-
 let adventurer;
 
 function Adventurer (classType, hp, mp, str, dex, int, xp) {
@@ -9,18 +8,50 @@ this.mp= mp;
 this.str= str;
 this.dex=dex;
 this.int=int;
+this.atk= (str+dex+int)
 this.xp=xp;
-
 };
-let Warrior= new Adventurer("Warrior")
 
-let Mage= new Adventurer("Mage")
 
-let Theif= new Adventurer("Theif")
+
 
 let enemy;
 
-function Enemy (name, value, value, value) {
+function Enemy (name, hp, mp, atk, xp) {
+this.name= name;
+this.hp= hp;
+this.mp= mp;
+this.atk= atk;
+this.xp=xp;
+};
+
+
+let slime= new Enemy("Slime", 150, 20, 20, 10);
+
+let goblin= new Enemy("Goblin", 80, 10, 18, 20);
+
+let wyvern= new Enemy("Wyvern", 120, 50, 22, 35);
+
+switch (enemy) {
+  case 0:
+    enemy=slime;
+    break;
+
+  case 1:
+    enemy=goblin;
+    break;
+
+  case 2:
+    enemy=wyvern;
+    break;
+}
+
+currentEnemy=[slime, goblin, wyvern]
+
+
+let bossEnemy;
+
+function BossEnemy (name, value, value, value, value) {
 this.name= name;
 this.hp= (Math.floor(Math.random() *2)+4);
 this.mp= Math.random();
@@ -28,28 +59,16 @@ this.atk= (Math.floor(Math.random() *10)+5);
 this.xp=value;
 };
 
-let slime= new Enemy("Slime")
-let goblin= new Enemy("Goblin")
-let wyvern= new Enemy("Wyvern")
-let tekato= new Enemy("Tekato")
-let instructor= new Enemy("Intructor")
+let dragon= new BossEnemy("Dragon");
+let warewolf= new BossEnemy("Warewolf");
+let perscholas= new BossEnemy("Perscholas");
+
+let currBoss = [dragon, warewolf, perscholas];
+
+// my attack function
 
 
-let bossEnemy;
 
-function BossEnemy (name, value, value, value) {
-this.name= name;
-this.hp= (Math.floor(Math.random() *2)+4);
-this.mp= Math.random();
-this.str= (Math.floor(Math.random() *10)+5);
-this.dex=value;
-this.int=value;
-this.xp=value;
-};
-
-let dragon= new BossEnemy("Dragon")
-let warewolf= new BossEnemy("Warewolf")
-let perscholas= new BossEnemy("Perscholas")
 
 
 
@@ -57,33 +76,33 @@ let gameManager={
   setGameStart: function(classType){
     this.resetPlayer(classType);
     this.setPreFight();
-
   },
   resetPlayer: function(classType){
     switch (classType) {
       case "Warrior":
-        adventurer = new Adventurer(Warrior, 250, 100, 45, 5, 0, 0);
+        adventurer = new Adventurer("Warrior", 250, 100, 45, 5, 0, 0);
         break;
 
-      case "Theif":
-          adventurer = new Adventurer(Theif, 250, 100, 10, 45, 0, 0);
+      case "Thief":
+          adventurer = new Adventurer("Thief", 250, 100, 10, 45, 0, 0);
           break;
 
       case "Mage":
-            adventurer = new Adventurer(Mage, 250, 200, 0, 5, 60, 0);
+            adventurer = new Adventurer("Mage", 250, 200, 0, 5, 60, 0);
             break;
     }
 
     let getInterface = document.getElementById('interface').innerHTML=
     '<div>' +
     '<h3>'  + classType + '</h3>' +
-    '<p> HP:  ' + adventurer.hp + ' </p>' +
-    '<p> MP:  ' + adventurer.mp + '</p>' +
+    '<p id="playerHp"> HP:  ' + adventurer.hp + ' </p>' +
+    // '<p> MP:  ' + adventurer.mp + '</p>' +
     '<p> STR: ' + adventurer.str + '</p>' +
     '<p> DEX: ' + adventurer.dex + '</p>' +
     '<p> INT: ' + adventurer.int + '</p>' +
     '<p> XP:  ' + adventurer.xp + '</p>' +
     '</div>';
+
   },
   setPreFight: function(){
 
@@ -91,53 +110,158 @@ let gameManager={
                     document.getElementById('preGame').style.display="none";
   }
 }
+let gameOver = () =>{
+if (adventurer.hp<=0){
+
+  document.getElementById('game').style.display='none';
+  document.getElementById('battleScreen').style.display="none"
+  document.getElementById('interface').innerHTML='<h1 class="gameOver">GameOver</h1>'
+}
+}
+
+
 // battleScreen
 
+let myAtk = () => {
+
+
+if (slime.hp >= 1 && adventurer.hp > 0){
+
+   slime.hp -= adventurer.atk
+
+   document.getElementById('enemyHp')
+   enemyHp.innerText =`Hp: ${slime.hp}`;
+
+   if (slime.hp >= 100){
+   enemyHp.style.color="green"
+ }
+    if (slime.hp <=99 ){
+    enemyHp.style.color="red"
+    }
+   if(slime.hp< 0)
+   {
+   return slime.hp = 0;
+   }
+ }
+}
+
+
+// enemy attack function
+
+      let enAtk = () => {
+
+        if (adventurer.hp>=1 && slime.hp>0){
+
+              adventurer.hp -= slime.atk
+              document.getElementById('playerHp').innerText =`HP: ${adventurer.hp}`;
+              let playerHp=document.getElementById('playerHp');
+
+              if (adventurer.hp>=160){
+              playerHp.style.color="green";
+            }
+              if (adventurer.hp<=159 && adventurer.hp>80){
+              playerHp.style.color="orange";
+            }
+              if (adventurer.hp<80){
+              playerHp.style.color="red";
+            }
+
+
+            }
+          }
+
+
+
+// what happens on enemy death
+
+
+
+
+
+// initializes on attack button press
+
+
+
+let atkFun = () => {
+  let atkBtn= document.getElementById('atkBtn')
+
+
+myAtk();
+enAtk();
+gameOver();
+  };
+
 // battleFunctions
+let returnFunc = () => {
+  goBack = document.getElementById('returnBtn')
+  if (slime.hp<=0){
+
+  document.getElementById('game').style.display='block';
+  document.getElementById('battleScreen').style.display="none"
+  document.getElementById('stats').style.display="none"
+
+  return slime.hp=150;
+}
+}
 
 
-
-
+// game map key:
+// 0 = wall
+// 1 = path
+// 2 = fights
+// 3 = boss
 
 let ctx = null;
 let gameMap = [
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 2, 2, 0,
-	0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 2, 2, 0,
-	0, 1, 1, 1, 0, 0, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 0,
-	0, 1, 1, 1, 1, 0, 0, 1, 2, 1, 1, 2, 1, 1, 2, 1, 0, 0, 0, 0,
-	0, 2, 3, 1, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 0,
-	0, 1, 1, 1, 1, 2, 0, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 0,
-	0, 1, 1, 1, 1, 2, 0, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 0,
-	0, 1, 1, 1, 1, 2, 0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 1, 0,
-	0, 1, 1, 1, 1, 2, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 2, 1, 0,
-	0, 1, 2, 2, 2, 2, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 2, 1, 0,
-	0, 1, 2, 1, 1, 2, 1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 2, 1, 3,
-	0, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 0,
-	0, 1, 2, 1, 0, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 0, 1, 2, 1, 0,
-	0, 2, 2, 1, 0, 0, 1, 2, 2, 2, 2, 2, 2, 2, 1, 0, 1, 2, 1, 0,
-	0, 2, 2, 1, 0, 0, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 0,
-	0, 1, 2, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 0,
-	0, 1, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 0,
+	0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 2, 2, 0,
+	0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 2, 2, 0,
+	0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 2, 0, 1, 1, 2, 2, 2, 2, 2, 0,
+	0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+	0, 2, 0, 1, 0, 2, 2, 2, 0, 1, 1, 2, 0, 2, 0, 1, 1, 1, 1, 0,
+	0, 1, 0, 1, 1, 2, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0,
+	0, 1, 0, 1, 1, 2, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0,
+	0, 1, 1, 1, 0, 2, 0, 0, 0, 0, 0, 1, 0, 1, 0, 2, 0, 2, 1, 0,
+	0, 1, 0, 0, 0, 2, 1, 2, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0,
+	0, 1, 2, 2, 2, 2, 1, 2, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0,
+	0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 2,
+	0, 1, 2, 1, 0, 2, 1, 2, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0,
+	0, 1, 0, 1, 0, 2, 0, 0, 0, 1, 0, 0, 0, 1, 2, 0, 1, 2, 1, 0,
+	0, 2, 2, 1, 0, 1, 1, 2, 2, 2, 2, 2, 0, 1, 0, 0, 0, 0, 1, 0,
+	0, 2, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0,
+	0, 1, 0, 1, 1, 1, 1, 2, 1, 1, 1, 1, 0, 2, 0, 1, 0, 0, 1, 0,
+	0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0,
 	0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 0,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 ];
+// height and width of boxes
 let boxW = 40, boxH = 40;
+
+// height and width of the map itself
 let mapW = 20, mapH = 20;
+
+//rendering and movement speed
+//declared to be used later
 let currentSecond = 0, frameCount = 0, framesLastSecond = 0, lastFrameTime = 0;
 
+// types of boxes or tiles in this game
 let floorTypes = {
 	solid	: 0,
 	path	: 1,
-	fight : 2,
+  fight : 2,
+  win: 3
 
 };
 
-let boxEvents = {
+// event boxes have an undefined function until stepped on
+// so they wont process on start up like a place holder
+// the keys of this object are tiles
+//   to find toIndex
+//      (y * mapW) + x)
 
-  101:function(c){c},
-  102:function(c){c}
-};
+
+
+
 
 // to find toIndex
 //       (y * mapW) + x)
@@ -145,43 +269,74 @@ let boxEvents = {
 //0 is still first in array coords
 
 
-function battleBox()
+// the function to be passed into boxEvents when activated
+
+
+function battleBox(){
+
 // can do or statements to add more cords
-{ if (gameMap[toIndex(5,1)]==gameMap[toIndex(5,1)] ||
-      gameMap[toIndex(5,2)]==gameMap[toIndex(5,2)])
-      {
+ if (gameMap[toIndex(8,3)]==gameMap[toIndex(8,3)] ||
+gameMap[toIndex()]==gameMap[toIndex(5,1)])
+ {
+   let enemy= new Enemy("Slime", 100, 20 ,20 ,20)
 
 
-  // generate new enemy
-  // if new enemy health > 0
 
+
+    // what display changes are made when a fight event occurs
+    document.getElementById('stats').style.display="block"
+    let slimeImg= document.getElementById('slimeImg');
+    slimeImg.src="images/slime.png";
+    document.getElementById('enemyName').innerText =` ${slime.name}`;
+    document.getElementById('enemyHp').innerText =(`Hp: ${slime.hp}`);
+    document.getElementById('enemyAtk').innerText =`ATK: ${slime.atk}`;
+    document.getElementById('enemyXp').innerText =`xp: ${slime.xp}`;
     document.getElementById('game').style.display='none';
     document.getElementById('battleScreen').style.display="inline"
     document.getElementById('battleScreen').innerHTML="<h1>BATTLE</h2>" +
-
-    '<div>' +
-    '<h3>'  + name + '</h3>' +
-    '<p> HP:  ' + slime.hp + ' </p>' +
-    '<p> MP:  ' + slime.mp + '</p>' +
-    '<p> STR: ' + slime.str + '</p>' +
-    '<p> XP:  ' + slime.xp + '</p>' +
-    '</div>';
-
-    // let hp = 3;
-    // for(let hp = 3; hp>0; hp--)
-    console.log("hey");
-    // return true;
-}
+    `<button id="atkBtn" onclick="atkFun()">attack</button>` +
+    `<button id="returnBtn" onclick="returnFunc()">return</button>`
 
 }
+};
 
+function winBox(){
+if (gameMap[toIndex(11,18)]===gameMap[toIndex(11,18)]){
+
+ document.getElementById('game').style.display='none';
+ document.getElementById('battleScreen').style.display="none"
+ document.getElementById('interface').innerHTML='<h1 class="gameOver">You Have Won...</h1>'
+ }
+};
+
+
+let boxEvents = {
+  101:enemy,
+  101:function(){},
+  163:enemy,
+  163:function(){},
+
+}
+
+let winEvents = {
+  239:function(){},
+  238:function(){},
+}
+// change color and behavior of the boxes
+// solid or path is all I currently have
 
 let boxTypes = {
 	0 : { colour:"#685b48", floor:floorTypes.solid	},
 	1 : { colour:"#5aa457", floor:floorTypes.path	},
-	2 : { colour:"#e8bd7a", floor:floorTypes.path	},
+	2 : { colour:"#5aa457", floor:floorTypes.path	},
 	3 : { colour:"#286625", floor:floorTypes.path	}
 };
+
+// controls for the game
+// up
+// down
+// left
+// right
 
 let keysDown = {
 	37 : false,
@@ -189,6 +344,11 @@ let keysDown = {
 	39 : false,
 	40 : false
 };
+
+// an attemp to make the screen stop moving
+// added viewport pretty much makes the game move
+// around the player and makes the player position static
+
 
 let viewport = {
 	screen		: [0,0],
@@ -215,6 +375,16 @@ let viewport = {
 	}
 };
 
+
+// in this case our player/Character
+// is the square:
+// where it wants to move
+// when it moves
+// size height and width
+// position in the box
+// position on the grid(centered)
+// movement speed
+
 let player = new Character();
 
 function Character()
@@ -224,8 +394,10 @@ function Character()
 	this.timeMoved	= 0;
 	this.dimensions	= [30,30];
 	this.position	= [45,45];
-	this.delayMove	= 700;
+	this.delayMove	= 200;
 }
+// position inside of square
+
 Character.prototype.placeAt = function(x, y)
 {
 	this.boxFrom	= [x,y];
@@ -233,6 +405,9 @@ Character.prototype.placeAt = function(x, y)
 	this.position	= [((boxW*x)+((boxW-this.dimensions[0])/2)),
 		((boxH*y)+((boxH-this.dimensions[1])/2))];
 };
+
+// when it moves
+
 Character.prototype.processMovement = function(t)
 {
 	if(this.boxFrom[0]==this.boxTo[0] && this.boxFrom[1]==this.boxTo[1]) { return false; }
@@ -246,7 +421,15 @@ Character.prototype.processMovement = function(t)
 
     {
       battleBox();
+
     }
+    if(typeof winEvents[toIndex(this.boxTo[0], this.boxTo[1])]!='undefined'||
+    typeof winEvents[toIndex(this.boxFrom[0], this.boxFrom[1])]!='undefined')
+
+      {
+        winBox();
+
+      }
 
   }
   else
@@ -271,6 +454,10 @@ Character.prototype.processMovement = function(t)
 
 	return true;
 }
+
+// where it wants to move
+//checks to see if it can
+// if yes this control key moves 1 space in this instance
 Character.prototype.canMoveTo = function(x, y)
 {
 	if(x < 0 || x >= mapW || y < 0 || y >= mapH || battleScreen==true) { return false; }
@@ -287,17 +474,26 @@ Character.prototype.moveRight	= function(t) { this.boxTo[0]+=1; this.timeMoved =
 Character.prototype.moveUp		= function(t) { this.boxTo[1]-=1; this.timeMoved = t; };
 Character.prototype.moveDown	= function(t) { this.boxTo[1]+=1; this.timeMoved = t; };
 
+
+// coordinates function
+
 function toIndex(x, y)
 {
 	return((y * mapW) + x);
 }
 
+
+
+// what i want to happen when the page loads up
+// what do i want to show what do i want hidden
+// what functions if any and so on
+
 window.onload = function()
 {
-  document.getElementById("battleScreen").style.display = "none";
   document.getElementById("game").style.display = "none";
 
-  // return gameManager
+
+
 
 	ctx = document.getElementById('game').getContext("2d");
 	requestAnimationFrame(drawGame);
@@ -313,6 +509,12 @@ window.onload = function()
 	viewport.screen = [document.getElementById('game').width,
 		document.getElementById('game').height];
 };
+
+
+// in this function is where viewport plays inspect in
+// it draws the area within a radius saving power
+// seeing the only the part of the map that you are on
+// instead of the whole map or having ofscreen areas
 
 function drawGame()
 {
@@ -355,13 +557,33 @@ function drawGame()
 		}
 	}
 
-	ctx.fillStyle = "#0000ff";
+  // player box fill rectangle
+  // color
+
+	ctx.fillStyle = "black";
 	ctx.fillRect(viewport.offset[0] + player.position[0], viewport.offset[1] + player.position[1],
 		player.dimensions[0], player.dimensions[1]);
 
-	ctx.fillStyle = "#ff0000";
+	ctx.fillStyle = "#fff000";
 
 
 	lastFrameTime = currentFrameTime;
 	requestAnimationFrame(drawGame);
 }
+
+let stats = () => {
+
+      document.getElementById('enemyName').innerText =` ${slime.name}`;
+
+      let enemyHp=document.getElementById('enemyHp');
+      enemyHp.innerHTML =`<h3 id='redT'> Hp:` + slime.hp + `</h3>`;
+      enemyHp.style.color="red";
+      document.getElementById('enemyAtk').innerText =`ATK: ${slime.atk}`;
+      document.getElementById('enemyXp').innerText =`xp: ${slime.xp}`;
+
+
+
+      // document.getElementById('myHull').innerText = `Ship Health: ${adventurer.}`;
+      // document.getElementById('shipName').innerText = `Ship: ${player.name}`;
+}
+console.log(slime.name)
